ad36ebd0edf46f928ee107baf138c91c76da345d
pfernandes <phelipe.fern@gmail.com>
2019-10-30 00:01:33 -0400

    add email functionality

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-$(document).ready(function () {
-    $('.form-submit').click(function (e) {
+$(document).ready(function() {
+  $(".form-submit").click(function(e) {
+    // e.preventDefault();
+    let name = $(".name").val();
+    let email = $(".email").val();
+    let subject = $(".subject").val();
+    let message = $(".message").val();
+
+    function emailIsValid(email) {
+      return /\S+@\S+\.\S+/.test(email);
+    }
+
+    if (emailIsValid(email)) {
+    }
+    if (subject.length > 2) {
+    }
+    if (message.length > 2) {
+    }
+    if (name.length > 0) {
+    } else {
         e.preventDefault();
-        let name = $('.name').val()
-        let email = $('.email').val()
-        let subject = $('.subject').val()
-        let message = $('.message').val()
-
-        function emailIsValid(email) {
-          return /\S+@\S+\.\S+/.test(email);
-        }
-
-        if(emailIsValid(email)) {
-
-        } else {
-            alert("Invalid email credentials.")
-        }
-
-    })
-})
+      alert(
+        "Invalid email credentials. Please make sure to include a valid email, a subject line greater than two characters and a message."
+      );
+    }
+  });
+});
